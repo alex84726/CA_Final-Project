36abdb28a85b5d1cd3e709881d60bbeed37a3a41
alexhsu <b02901053@ntu.edu.tw>
2016-12-26 22:30:50 +0800

    put project 1 file in, Notice Data_Memory

---
+++ Project2/dcache_one_way_example/Forwarding_unit.v
@@ -0,0 +1,53 @@
+module Forwarding_unit(
+    clk_i,
+    MEM_Rd_i,
+    WB_Rd_i,
+    MEM_W_i, 
+    WB_W_i,
+    RS_i, 
+    RT_i,
+    RS_Src_o,
+    RT_Src_o 
+);
+
+// Ports
+input               clk_i;
+input   [4:0]       MEM_Rd_i;
+input   [4:0]       WB_Rd_i;
+input               MEM_W_i;
+input               WB_W_i;
+input   [4:0]       RS_i;
+input   [4:0]       RT_i;
+output  reg [1:0]       RS_Src_o; 
+output  reg [1:0]       RT_Src_o;
+
+//RS_Src_o
+always @(*) begin
+    // EX hazard
+    if((MEM_W_i == 1'b1)&&(MEM_Rd_i != 5'b0)&&(MEM_Rd_i == RS_i)) begin
+      RS_Src_o = 2'b10;
+    end
+    // MEM hazard
+    else if((WB_W_i == 1'b1)&&(WB_Rd_i != 5'b0)&&(MEM_Rd_i != RS_i)&&(WB_Rd_i == RS_i)) begin
+      RS_Src_o = 2'b01;
+    end
+    else begin
+      RS_Src_o = 2'b00;
+    end
+end
+
+//RT_Src_o
+always @(*) begin
+    // EX hazard
+    if((MEM_W_i == 1'b1)&&(MEM_Rd_i != 5'b0)&&(MEM_Rd_i == RT_i)) begin
+      RT_Src_o = 2'b10;
+    end
+    else if((WB_W_i == 1'b1)&&(WB_Rd_i != 5'b0)&&(MEM_Rd_i != RT_i)&&(WB_Rd_i == RT_i)) begin
+      RT_Src_o = 2'b01;
+    end
+    else begin
+      RT_Src_o = 2'b00;
+    end
+end
+   
+endmodule 
